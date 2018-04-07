fb3ea7f8e6c56e560cb2d8dbf67c1a01829164ca
Hendri <hendri.gnw@gmail.com>
2018-04-07 11:16:13 +0700

    quis pertemuan 4

---
+++ src/QuisPertemuan4GajiKaryawan/QuisPertemuan4GajiKaryawan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QuisPertemuan4GajiKaryawan;
+
+/**
+ *
+ * @author FASILKOM
+ */
+public class QuisPertemuan4GajiKaryawan {
+    /**
+     * Buatlah program GUI java untuk menghitung gaji karyawan berikut ini:
+     * perhitungan gaji karyawan tetap
+     * gaji pokok 1.730.000
+     * lembur 1 jam @100.000
+     * transport perhari 50.000
+     * total gaji = gaji pokok + uang lembur + uang transport
+     * dikurang dengan iuran bpjs kelas 1 : 1 orang 80.000
+     * gaji yang dibayarkan = total gaji - iuran bpjs
+     * 
+     * perhitungan gaji karyawan honorer:
+     * perjam 150.000 (1 hari 8 jam,  1 minggu 3x masuk kerja)
+     * transport 30.000 /hari
+     * total gaji = gaji honorer + transport
+     */
+}
